b93a3276c66ed7309b90cdd5332cabfa88b866ab
Albina Toktieva <mirasafa.lol@gmail.com>
2025-06-03 17:28:44 +0500

    Убрала параметризацию с теста

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -1,7 +1,4 @@
-import pytest
-
-from data import INGREDIENT_PRICE, INGREDIENT_TYPE, INGREDIENT_NAME, INGREDIENT_TYPE_1, INGREDIENT_TYPE_2
-from praktikum.ingredient import Ingredient
+from data import INGREDIENT_PRICE, INGREDIENT_TYPE, INGREDIENT_NAME
 
 
 class TestIngredient:
@@ -14,13 +11,6 @@ class TestIngredient:
     def test_get_name_success(self, ingredient):
         assert ingredient.get_name() == INGREDIENT_NAME
 
-    @pytest.parametrize(
-        'ingredient_type',
-    [
-        INGREDIENT_TYPE,
-        INGREDIENT_TYPE_1,
-        INGREDIENT_TYPE_2
-    ])
-    def test_get_type_success(self, ingredient_type):
-        ingredient = Ingredient(name=INGREDIENT_NAME, price=INGREDIENT_PRICE, ingredient_type=ingredient_type)
+
+    def test_get_type_success(self, ingredient):
         assert ingredient.get_type() == INGREDIENT_TYPE
