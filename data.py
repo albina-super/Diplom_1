0336785c90861e52bda1e01b080028387c9ce256
Albina Toktieva <mirasafa.lol@gmail.com>
2025-06-04 16:01:16 +0500

    Добавила тест на рецепт и добавила все необходимое для этого

--- data.py
+++ data.py
@@ -1,9 +1,22 @@
+# данные для булок
 BUN_NAME = 'Белая булочка'
 BUN_PRICE = 40
 
-
+# данные для ингредиентов
 INGREDIENT_PRICE = 20
 INGREDIENT_NAME = 'Огурцы'
 INGREDIENT_TYPE = 'Овощи'
 INGREDIENT_TYPE_1 = 'Мясо'
 INGREDIENT_TYPE_2 = ''
+
+
+# данные для булки рецепта бургера
+receipt_data_names = ['Чёрная булка', 'Хлеб', 'Сухарики']
+receipt_data_prices = [100, 10]
+
+
+receipt_data_names_2 = ['Белая булка', 'Сулугуни', 'Сыры']
+receipt_data_prices_2 = [120, 20]
+
+receipt_data_names_3 = ['Ржаная булка', 'Салат', 'Зелень']
+receipt_data_prices_3 = [150, 30]
--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,7 +1,7 @@
-from unittest.mock import Mock
-
 import pytest
 
+from data import receipt_data_names, receipt_data_prices, receipt_data_names_2, receipt_data_prices_2, \
+    receipt_data_names_3, receipt_data_prices_3
 
 
 class TestBurger:
@@ -16,6 +16,7 @@ class TestBurger:
         burger.add_ingredient(mock_ingredient)
         assert mock_ingredient in burger.ingredients
 
+
     @pytest.mark.parametrize('index', [0, 1])
     def test_remove_ingredient_success(self, burger, index, mock_ingredient, mock_ingredient_2, mock_ingredient_3):
         burger.add_ingredient(mock_ingredient)
@@ -23,12 +24,31 @@ class TestBurger:
         burger.add_ingredient(mock_ingredient_3)
         removed = burger.ingredients[index]
         initial_len = len(burger.ingredients)
-        print(initial_len, 'len')
         burger.remove_ingredient(index)
         assert len(burger.ingredients) == initial_len - 1 and removed not in burger.ingredients
 
-    def test_move_ingredient_success(self):
-       pass
+
+    @pytest.mark.parametrize('names, prices, result', [
+        (receipt_data_names, receipt_data_prices, '== Чёрная булка =='),
+        (receipt_data_names_2, receipt_data_prices_2, '= сыры Сулугуни ='),
+        (receipt_data_names_3, receipt_data_prices_3, 'Price: 330')
+    ])
+
+    def test_get_receipt_success(self, burger, mock_bun, mock_ingredient, result, names, prices):
+
+        mock_bun.get_name.return_value = names[0]
+        mock_bun.get_price.return_value = prices[0]
+        mock_ingredient.get_name.return_value = names[1]
+        mock_ingredient.get_price.return_value = prices[1]
+        mock_ingredient.get_type.return_value = names[2]
+        burger.add_ingredient(mock_ingredient)
+        burger.set_buns(mock_bun)
+        receipt = burger.get_receipt()
+
+        assert result in receipt
+
+
+
 
     @pytest.mark.parametrize(
         ('bun_price', 'ingredient_price', 'result'),
@@ -46,5 +66,16 @@ class TestBurger:
         assert sum_price == result
 
 
-    def test_get_receipt_success(self):
-        pass
+    @pytest.mark.parametrize(
+        ('index', 'new_index'),
+        [(0, 1), (1, 2)]
+    )
+    def test_move_ingredient_success(self, index, new_index, mock_ingredient, mock_ingredient_2, mock_ingredient_3, burger):
+        burger.add_ingredient(mock_ingredient_2)
+        burger.add_ingredient(mock_ingredient_3)
+        burger.add_ingredient(mock_ingredient)
+        ingredient_before_move = burger.ingredients[index]
+        burger.move_ingredient(index, new_index)
+        ingredient_after_move = burger.ingredients[new_index]
+
+        assert ingredient_before_move == ingredient_after_move
